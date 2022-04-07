18f983d50bcbc8b4933d6840f34b11cfa27e4768
danielcsmg <daniel.csouzamg@gmail.com>
2022-04-07 15:45:18 -0300

    Exercícios Desafios

--- src/com/company/MiniDesafioDois.java
+++ src/com/company/MiniDesafioDois.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class MiniDesafioDois {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
